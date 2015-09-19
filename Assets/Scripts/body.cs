e1c3c4dd997f3255253352909e2459541e139ac3
Ian King <gnik.nai@gmail.com>
2015-09-18 19:27:27 -0600

    Initial commit

---
+++ Assets/Scripts/body.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class body : MonoBehaviour {
+
+
+	string bodyName;
+	public Vector3 dn;
+	public Vector3 dvn;
+	public Vector3 acceleration;
+	public float mass;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
